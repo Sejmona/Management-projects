e2779dfcd464fd9f1db263ecb5444c1117af1a34
Simona Šmelhausová <101262454+Sejmona@users.noreply.github.com>
2024-10-17 14:24:17 +0200

    Creating URL, view and template

--- projektSprava/ukoly/views.py
+++ projektSprava/ukoly/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Ukol
 
-# Create your views here.
+@login_required
+def seznam_ukolu(request):
+    ukoly = Ukol.objects.filter(vlastnik=request.user)
+    return render(request, 'ukoly/seznam_ukolu.html', {'ukoly': ukoly})
+
